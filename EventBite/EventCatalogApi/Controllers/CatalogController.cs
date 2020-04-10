66c728496a797152fe91a7eb28e98b52e797d775
sky0cean <sky0cean@hotmail.com>
2020-04-09 17:39:58 -0700

    Added ViewModel

--- EventBite/EventCatalogApi/Controllers/CatalogController.cs
+++ EventBite/EventCatalogApi/Controllers/CatalogController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventCatalogApi.Data;
+using EventCatalogApi.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EventCatalogApi.Controllers
 {
@@ -11,5 +15,37 @@ namespace EventCatalogApi.Controllers
     [ApiController]
     public class CatalogController : ControllerBase
     {
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery]int pageSize = 10)
+        {
+
+            var items = await _context.CatalogItems
+                               .Skip(pageIndex * pageSize)
+                               .Take(pageSize)
+                               .ToListAsync();
+            items = ChangePictureUrl(items);
+            return Ok(items);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(
+                c => c.PictureUrl =
+                    c.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced",
+                        _config["ExternalCatalogBaseUrl"])
+                    );
+            return items;
+        }
     }
 }
---
+++ EventBite/EventCatalogApi/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogApi.ViewModels
+{
+    public class PaginatedItemsViewModel
+    {
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public long Count { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
