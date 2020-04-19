acca18ed5f1a9577983323e0953a61c67c31c089
sky0cean <sky0cean@hotmail.com>
2020-04-19 16:47:37 -0700

    Added ICatalogService on WebMVC

--- WebMVC/Services/ICatalogService.cs
+++ WebMVC/Services/ICatalogService.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Models;
 
 namespace WebMVC.Services
 {
     interface ICatalogService
     {
-        Task<> GetCatalogItemsAsync(int page, int size);
+        Task<Catalog> GetCatalogItemsAsync(int page, int size);
     }
 }
