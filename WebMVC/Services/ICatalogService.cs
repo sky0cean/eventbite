cffdc340581d64bcf0db53603073a8bd229122dc
sky0cean <sky0cean@hotmail.com>
2020-04-20 15:42:49 -0700

    Added CatalogController

--- WebMVC/Services/ICatalogService.cs
+++ WebMVC/Services/ICatalogService.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMVC.Models;
 
 namespace WebMVC.Services
 {
     public interface ICatalogService
     {
-        Task<> GetCatalogItemsAsync(int page, int size);
+        Task<Catalog> GetCatalogItemsAsync(int page, int size);
     }
 }
--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebMVC.Infrastructure;
 
 namespace WebMVC
 {
@@ -24,6 +25,7 @@ namespace WebMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IHttpClient, CustomHttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
