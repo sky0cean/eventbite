da1d56e21108210d6989632e1540014e138ca93d
sky0cean <sky0cean@hotmail.com>
2020-04-20 14:46:06 -0700

    finished March 22 part

---
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class CustomHttpClient : IHttpClient
+    {
+        public Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
