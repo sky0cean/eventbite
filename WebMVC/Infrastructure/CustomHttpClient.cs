f9af679dc038178d59cff252b04b7a68c825a09b
sky0cean <sky0cean@hotmail.com>
2020-04-20 15:20:03 -0700

    Added WebMVC/Models

--- WebMVC/Infrastructure/CustomHttpClient.cs
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -8,17 +8,32 @@ namespace WebMVC.Infrastructure
 {
     public class CustomHttpClient : IHttpClient
     {
-        public Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        private readonly HttpClient _client;
+        public CustomHttpClient()
+        {
+            _client = new HttpClient();
+        }
+        public async Task<string> GetStringAsync(string uri,
+            string authorizationToken = null, string authorizationMethod = "Bearer")
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            if(authorizationToken != null)
+            {
+                //Do this after token service
+            }
+
+            var response = await _client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri,
+            T item, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             throw new NotImplementedException();
         }
 
-        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public Task<HttpResponseMessage> PutAsync<T>(string uri,
+            T item, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             throw new NotImplementedException();
         }
