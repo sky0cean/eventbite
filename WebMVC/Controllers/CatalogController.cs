a7e7947b001d27199bb4edcb3bd563089be9d753
sky0cean <sky0cean@hotmail.com>
2020-04-21 10:54:55 -0700

    Added temporary view

--- WebMVC/Controllers/CatalogController.cs
+++ WebMVC/Controllers/CatalogController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Services;
 
 namespace WebMVC.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Index()
+        private readonly ICatalogService _service;
+        public CatalogController(ICatalogService service)
         {
-            return View();
+            _service = service;
+        }
+        public async Task<IActionResult> Index(int page)
+        {
+
+            var itemsOnPage = 10;
+
+           var catalog = await _service.GetCatalogItemsAsync(page, itemsOnPage);
+
+            return View(catalog);
         }
     }
 }
