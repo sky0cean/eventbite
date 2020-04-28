ad6bcb06284f8f767b94b2d7dee866efe6864375
sky0cean <sky0cean@hotmail.com>
2020-04-28 14:11:48 -0700

    Fixed errors

--- EventBite/EventCatalogApi/Migrations/20200428203845_Second.Designer.cs
+++ EventBite/EventCatalogApi/Migrations/20200428203845_Second.Designer.cs
@@ -3,14 +3,16 @@ using EventCatalogApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventCatalogApi.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class CatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20200428203845_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventBite/EventCatalogApi/Migrations/20200428203845_Second.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventCatalogApi.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "CatalogPrices",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Catalog",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "CatalogPrices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Catalog",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
